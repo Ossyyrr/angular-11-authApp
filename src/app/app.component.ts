b4513e16b69fab4753714f3a2af6d317be93b285
Patricia Manzanero Jiménez <52755910+Ossyyrr@users.noreply.github.com>
2024-03-22 13:42:29 +0100

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, inject } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthStatus } from './auth/interfaces';
+import { AuthService } from './auth/service/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = '11-authApp';
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  public finishedAuthCheck = computed<boolean>(() => {
+    console.log(this.authService.authStatus());
+    if (this.authService.authStatus() === AuthStatus.checking) {
+      return false;
+    }
+    return true;
+  });
+
+  public authStatusChangedEffect = effect(() => {
+    //! Un efecto se ejecuta la primera vez y cada vez que cambia el valor de las dependencias
+    switch (this.authService.authStatus()) {
+      case AuthStatus.checking:
+        return;
+
+      case AuthStatus.authenticated:
+        this.router.navigateByUrl('/dashboard');
+        return;
+
+      case AuthStatus.notAuthenticated:
+        this.router.navigateByUrl('/auth/login');
+        return;
+    }
+  });
 }
