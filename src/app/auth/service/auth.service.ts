91be8006bdcb3cd77ad6b45db166f37b792ca5e1
Patricia Manzanero Jiménez <52755910+Ossyyrr@users.noreply.github.com>
2024-03-22 13:03:33 +0100

    wip

--- src/app/auth/service/auth.service.ts
+++ src/app/auth/service/auth.service.ts
@@ -1,9 +1,28 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, computed, inject, signal } from '@angular/core';
+import { Observable } from 'rxjs';
+import { enviroment } from '../../../enviroments/enviroments';
+import { AuthStatus, User } from '../interfaces';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private readonly baseUrl: string = enviroment.baseUrl;
+  private http = inject(HttpClient);
 
-  constructor() { }
+  private _currentUser = signal<User | null>(null);
+  private _authStatus = signal<AuthStatus>(AuthStatus.checking);
+
+  public currentUser = computed(() => this._currentUser());
+  public authStatus = computed(() => this._authStatus());
+
+  constructor() {}
+
+  login(email: string, password: string): Observable<boolean> {
+    return this.http.post<boolean>(`${this.baseUrl}/login`, {
+      email,
+      password,
+    });
+  }
 }
