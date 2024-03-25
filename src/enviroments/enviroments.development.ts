ae36f4b129cefc81e67720676d5657f7297ec6e3
Patricia Manzanero Jiménez <52755910+Ossyyrr@users.noreply.github.com>
2024-03-25 12:35:20 +0100

    fileReplacements

---
+++ src/enviroments/enviroments.development.ts
@@ -0,0 +1,4 @@
+export const enviroment = {
+  production: true,
+  baseUrl: 'https://angular-nest-11-nest-backend-production.up.railway.app',
+};
--- src/enviroments/enviroments.ts
+++ src/enviroments/enviroments.ts
@@ -2,3 +2,5 @@ export const enviroment = {
   production: false,
   baseUrl: 'http://localhost:3000',
 };
+
+//  https://angular-nest-11-nest-backend-production.up.railway.app/
